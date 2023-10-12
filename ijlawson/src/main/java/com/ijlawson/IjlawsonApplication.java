cb4d01b47ba3ee754b6bbc4e834f6356617e90d2
BuildTools <isaactexting@gmail.com>
2023-10-11 19:40:54 -0500

    Pushing basic project files

---
+++ ijlawson/src/main/java/com/ijlawson/IjlawsonApplication.java
@@ -0,0 +1,13 @@
+package com.ijlawson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjlawsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjlawsonApplication.class, args);
+	}
+
+}
